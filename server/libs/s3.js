a97773ea0d13313169c138fab2c825610d83e579
guanhongl <guanh.sl23@gmail.com>
2023-02-05 02:08:06 -1000

    init database
    redis, s3

---
+++ server/libs/s3.js
@@ -0,0 +1,7 @@
+const AWS = require("aws-sdk")
+
+const s3 = new AWS.S3({
+
+})
+
+module.exports = s3
--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,24 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const redis = require("redis")
+const client = redis.createClient({
+    socket: {
+        host: "localhost",
+        port: "6379"
+    }
+})
+client.on("error", err => {
+    console.log(`Redis Client Error ${err}`)
+})
+
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+client.connect()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    })
